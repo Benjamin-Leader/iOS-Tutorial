c1bf772ba4ee5951a424ad6c63f1713e1851b7cb
=Ben Leader <=>
2014-09-04 13:28:14 -0700

    Finished Part 1 of Tutorial

--- BullsEye/BLViewController.h
+++ BullsEye/BLViewController.h
@@ -10,4 +10,13 @@
 
 @interface BLViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UISlider *slider;
+@property (nonatomic, weak) IBOutlet UILabel *targetLabel;
+@property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *roundLabel;
+
+- (IBAction)showAlert;
+
+- (IBAction)sliderMoved:(UISlider *)slider;
+
 @end
