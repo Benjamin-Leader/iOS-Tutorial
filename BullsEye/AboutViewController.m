04fe1bd747328960f4a28cf502d990985d92a474
=Ben Leader <=>
2014-09-08 22:54:57 -0700

    Finished Tutorial

--- BullsEye/AboutViewController.m
+++ BullsEye/AboutViewController.m
@@ -14,4 +14,16 @@
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *htmlFile = [[NSBundle mainBundle]
+                          pathForResource:@"BullsEye" ofType:@"html"]; NSData *htmlData = [NSData dataWithContentsOfFile:htmlFile];
+    NSURL *baseURL = [NSURL fileURLWithPath:[
+                                             [NSBundle mainBundle] bundlePath]];
+    [self.webView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:baseURL];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES; }
+
 @end
--- BullsEye/BLViewController.m
+++ BullsEye/BLViewController.m
@@ -25,6 +25,20 @@
     [super viewDidLoad];
 	[self startNewGame];
     [self updateLabels];
+    
+    UIImage *thumbImageNormal = [UIImage imageNamed:@"SliderThumb-Normal"];
+    [self.slider setThumbImage:thumbImageNormal forState:UIControlStateNormal];
+    UIImage *thumbImageHighlighted = [UIImage imageNamed:@"SliderThumb-Highlighted"];
+    [self.slider setThumbImage:thumbImageHighlighted forState:UIControlStateHighlighted];
+    UIImage *trackLeftImage =
+    [[UIImage imageNamed:@"SliderTrackLeft"]
+     resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)]; [self.slider setMinimumTrackImage:trackLeftImage
+                                                                                           forState:UIControlStateNormal];
+    UIImage *trackRightImage =
+    [[UIImage imageNamed:@"SliderTrackRight"]
+     resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)];
+    [self.slider setMaximumTrackImage:trackRightImage forState:UIControlStateNormal];
+    
 }
 
 - (void)startNewRound
@@ -100,8 +114,11 @@
 
 - (IBAction)startOver
 {
-    [self startNewGame];
+    CATransition *transition = [CATransition animation]; transition.type = kCATransitionFade; transition.duration = 1;
+    transition.timingFunction = [CAMediaTimingFunction
+                                 functionWithName:kCAMediaTimingFunctionEaseOut]; [self startNewGame];
     [self updateLabels];
+    [self.view.layer addAnimation:transition forKey:nil];
 }
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
@@ -109,5 +126,8 @@
     [self startNewRound];
     [self updateLabels];
 }
+
+- (BOOL)prefersStatusBarHidden {
+    return YES; }
 
 @end
