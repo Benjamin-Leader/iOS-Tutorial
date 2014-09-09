62411169343b149636417761f480c391eeb0f1a7
=Ben Leader <=>
2014-09-08 19:16:23 -0700

    Info Screen Added

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Ben Leader on 9/8/14.
+//  Copyright (c) 2014 Benjamin Leader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
---
+++ BullsEye/AboutViewController.m
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.m
+//  BullsEye
+//
+//  Created by Ben Leader on 9/8/14.
+//  Copyright (c) 2014 Benjamin Leader. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+
+- (IBAction)close {
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
